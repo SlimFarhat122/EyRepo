2d8b72ff2755801f35c4fc46abba0b5889feec0f
slimfarhat123 <slim.farhat@esprit.tn>
2025-02-17 09:44:15 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import {React } from 'react';
+ import MainLayout from './layouts/MainLayout';
+import Navbar from '../src/components/navbar';
+import Hero from './components/Hero';
+import HomeCard from './components/HomeCard';
+import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
+import Jobpage , {jobLoder} from './pages/jobpage';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage'
+import JobsPages from './pages/JobsPages';
+import NotFoundPage from './pages/NotFoundPage';
+import AddJobPage from './pages/AddJobPage';
+import TestPage from './pages/TestPage';
+
+const addJob = async (newJob) => {
+  const res = await fetch('/api/jobs', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newJob),
+  });
+  return;
+};
+const deleteJob = async (id) => {
+  const res = await fetch(`/api/jobs/${id}`, {
+    method: 'DELETE',
+  });
+  return;
+};
+
+// Update Job
+const updateJob = async (job) => {
+  const res = await fetch(`/api/jobs/${job.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(job),
+  });
+  return;
+};
+
+
+
+const router = createBrowserRouter(createRoutesFromElements
+  (<Route path='/' element={<MainLayout/>}>
+  <Route index element ={<HomePage/>} />
+  <Route path='/jobs' element={<JobsPages/>}/>
+  <Route path='/Test' element={<TestPage/>}/>
+
+  <Route path='/jobs/:id' element={<Jobpage />} loader={jobLoder}/>
+  <Route path='/Add-job' element={<AddJobPage/>}/>
+
+  <Route path='/*' element={<NotFoundPage/>}/>
+
+  
+
+
+</Route>
+)
+
+
+)
+const App = () => {
+  return  <RouterProvider router = {router} />;
+};
+
+export default App;
